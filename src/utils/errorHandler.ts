506054f578753fc13eeab219289ec09d5e2794f8
João Carlos Magalhães <joaocarlos.m014@gmail.com>
2022-12-02 14:49:19 -0300

    refactor: simplify handleError, fix prisma schema

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -3,21 +3,20 @@ import { FastifyReply } from "fastify"
 import { ZodError } from "zod"
 import logger from "./logger/pino.js"
 
-export function handleError(error: Error) {
+export function handleError(reply: FastifyReply, error: any) {
   if (error instanceof ZodError) {
     const zodError = error.toString()
-    return logger.warn(`[Zod] ${zodError}`)
+    logger.warn(`[Zod] ${zodError}`)
+    return reply.code(422).send({ error: zodError })
   }
   if (error instanceof PrismaClientKnownRequestError) {
     const { code, message, name, cause, stack } = error
-    return logger.error(`[Prisma] ${code} ${message} ${name} ${cause} ${stack}`)
+    logger.error(`[Prisma] ${code} ${message} ${name} ${cause} ${stack}`)
+    return reply.code(400).send({ error: "Internal Server Error" })
   }
   if (error instanceof Error) {
     const errorMessage = JSON.stringify(error)
-    return logger.error(`[Error] ${errorMessage}`)
+    logger.error(`[Error] ${errorMessage}`)
+    return reply.code(500).send({ error: "Internal Server Error" })
   }
-}
-
-export function handleServerError(reply: FastifyReply, code: number, message: string) {
-  reply.code(code).send({ error: message })
 }
