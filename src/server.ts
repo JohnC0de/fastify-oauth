98b8ec6a3b7f8e36aee9a06058099694f3c873de
João Carlos Magalhães <joaocarlos.m014@gmail.com>
2022-12-12 19:55:56 -0300

    refactor: delegate errorHandler back to fastify

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import { fastify } from "fastify"
 import { oauthRoutes } from "./routes/oauth.routes.js"
 import { userRoutes } from "./routes/user.routes.js"
+import { handleError } from "./utils/errorHandler.js"
 import logger from "./utils/logger/pino.js"
 
 const server = fastify({ logger })
+
+server.setErrorHandler(function error() {})
 
 server.register(oauthRoutes, { prefix: "/oauth" })
 server.register(userRoutes)
